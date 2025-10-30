a4d7ebc2330e41ef68a57ab9289bfef880f129ec
Ryuta Kawamoto <ryu073000@i.softbank.jp>
2025-10-30 13:03:34 +0900

    plusのテストファイルにエラーを追加

--- test.bash
+++ test.bash
@@ -8,9 +8,19 @@ ng(){
 }
 
 res=0
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 
 [ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE  INPUT ###
+out=$(echo あ| ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
