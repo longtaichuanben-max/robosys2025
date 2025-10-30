cc2510ff14f6d9df33a9df85fd0531362f0340ff
Ryuta Kawamoto <ryu073000@i.softbank.jp>
2025-10-30 12:36:12 +0900

    plusのテストファイルに変更

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Ryuta Kawamoto ryu073000@i.softbank.jp
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng(){
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
+
