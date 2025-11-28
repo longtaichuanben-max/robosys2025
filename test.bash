700116cc1588b9cd3b2bba347324f725179f1376
Ryuta Kawamoto <ryu073000@i.softbank.jp>
2025-11-28 19:04:37 +0900

    課題のテストを作成

--- test.bash
+++ test.bash
@@ -9,16 +9,19 @@ ng(){
 
 res=0
 ### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
+out=$(echo "林檎 100" | ./mkcon)
+[ "${out}" = 100.0 ] || ng "$LINENO"
 
-[ "${out}" = 15 ] || ng "$LINENO"
+### ENPTY INPUT ###
+out=$(echo | ./mkcon)
+[ "$?" = 0 ] || ng "$LINENO"
 
 ### STRANGE  INPUT ###
-out=$(echo あ| ./plus)
+out=$(echo a a 100 | ./mkcon)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo  | ./plus)
+out=$(echo a a | ./mkcon)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
